f3b897c0279ec7bc22980d0d4276ba8a1dd5420f
KIRSTEN-23 <231296@virtualwindow.co.za>
2023-05-28 20:47:22 +0200

    Exercise 1

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var firstName = "Kirsten"
+let surname = "Venter"
+let age = "19"
+const subject = "Development"
+
+let message = "Hello, my name is " + firstName + " " + surname + ", I am " + age + " years old and my subject is " + subject
+
+console.log(message)
